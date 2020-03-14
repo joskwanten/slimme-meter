0f67e1e8d9003ae73e033865262ad485a27f385e
Jos Kwanten <jos.kwanten@gmail.com>
2020-03-14 17:31:01 +0000

    Added checksum

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module slimme-meter/reader
 
 require (
+	github.com/howeyc/crc16 v0.0.0-20171223171357-2b2a61e366a6
 	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
 	rsc.io/quote v1.5.2
--- reader.go
+++ reader.go
@@ -1,27 +1,88 @@
 package main
 
 import (
-      "github.com/tarm/serial"
-      "log"
+	"strconv"
+	"fmt"
+	"github.com/tarm/serial"
+	"log"
 )
 
-func main() {
-    c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
-    s, err := serial.OpenPort(c)
-	
+func crc16(buf []byte) uint {
+	var crc uint = 0
+	for pos:=0; pos<len(buf); pos++ {
+	  crc ^= uint(buf[pos])  // XOR byte into least sig. byte of crc
+	  
+	  for i :=8; i != 0; i-- {       // Loop over each bit
+		if (crc & 0x0001) != 0 {    // If the LSB is set
+		  crc >>= 1;                  // Shift right and XOR 0xA001
+		  crc ^= 0xA001;
+		} else {
+			                 // Else LSB is not set
+		  crc >>= 1;                  // Just shift right
+		}
+	  }
+	}
+	return crc;
+  }
+
+func ReadMessage(msgchan chan []byte) {
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
+	s, err := serial.OpenPort(c)
+
 	if err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 
 	for {
-    	buf := make([]byte, 2000)
-      	n, err := s.Read(buf)
-      	if err != nil {
-              log.Fatal(err)
-      	}
-		
-		log.Printf("%s", buf[:n])
-		//log.Printf("%d", n)
-		  
+		startFound := false
+		endFound := false
+		message := []byte{}
+
+		for !endFound {
+			buf := make([]byte, 2000)
+			n, err := s.Read(buf)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for i := 0; i < n; i++ {
+				if buf[i] == '!' {
+					endFound = true
+					message = append(message, buf[0:i + 1]...)
+					crc, err := strconv.ParseInt("0x"+string(buf[i+1:n-2]), 0, 32)
+					crcComp := int64(crc16(message))
+
+					if err == nil && crc == crcComp {
+						msgchan <- message
+					} else {
+						log.Printf("Failed to parse message")
+					}
+					break
+				}
+			}
+
+			if !startFound {
+				for i := 0; i < n; i++ {
+					if buf[i] == '/' {
+						startFound = true
+						message = append(buf[i:n-i])
+					}
+				}
+			} else {
+				message = append(message, buf[0:n]...)
+			}
+		}
 	}
+
+}
+
+func main() {
+	messages := make(chan []byte)
+	go ReadMessage(messages)
+	for {
+		message := <-messages
+
+		fmt.Printf("%s", message)
+	}
+	//log.Printf("%d", n)
 }
