b13860ec4356f2405b2dc9fef714a40ee6d2ba52
Jos Kwanten <jos.kwanten@gmail.com>
2020-03-14 19:20:48 +0000

    Added splitting of groups

--- reader.go
+++ reader.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-	"strconv"
 	"fmt"
 	"github.com/tarm/serial"
 	"log"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func crc16(buf []byte) uint {
 	var crc uint = 0
-	for pos:=0; pos<len(buf); pos++ {
-	  crc ^= uint(buf[pos])  // XOR byte into least sig. byte of crc
-	  
-	  for i :=8; i != 0; i-- {       // Loop over each bit
-		if (crc & 0x0001) != 0 {    // If the LSB is set
-		  crc >>= 1;                  // Shift right and XOR 0xA001
-		  crc ^= 0xA001;
-		} else {
-			                 // Else LSB is not set
-		  crc >>= 1;                  // Just shift right
+	for pos := 0; pos < len(buf); pos++ {
+		crc ^= uint(buf[pos]) // XOR byte into least sig. byte of crc
+
+		for i := 8; i != 0; i-- { // Loop over each bit
+			if (crc & 0x0001) != 0 { // If the LSB is set
+				crc >>= 1 // Shift right and XOR 0xA001
+				crc ^= 0xA001
+			} else {
+				// Else LSB is not set
+				crc >>= 1 // Just shift right
+			}
 		}
-	  }
 	}
-	return crc;
-  }
+	return crc
+}
 
 func ReadMessage(msgchan chan []byte) {
 	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
@@ -48,7 +50,7 @@ func ReadMessage(msgchan chan []byte) {
 			for i := 0; i < n; i++ {
 				if buf[i] == '!' {
 					endFound = true
-					message = append(message, buf[0:i + 1]...)
+					message = append(message, buf[0:i+1]...)
 					crc, err := strconv.ParseInt("0x"+string(buf[i+1:n-2]), 0, 32)
 					crcComp := int64(crc16(message))
 
@@ -65,7 +67,7 @@ func ReadMessage(msgchan chan []byte) {
 				for i := 0; i < n; i++ {
 					if buf[i] == '/' {
 						startFound = true
-						message = append(buf[i:n-i])
+						message = append(buf[i : n-i])
 					}
 				}
 			} else {
@@ -77,12 +79,24 @@ func ReadMessage(msgchan chan []byte) {
 }
 
 func main() {
+	r := regexp.MustCompile(`(?P<device>\d+-\d+):(?P<key>\d+.\d+.\d+)(?P<values>(\(.*?\))+)`)
 	messages := make(chan []byte)
 	go ReadMessage(messages)
 	for {
 		message := <-messages
 
-		fmt.Printf("%s", message)
+		//fmt.Printf("%s", message)
+		parts := strings.Split(string(message), "\r\n")
+		for i := 0; i < len(parts); i++ {
+			fmt.Printf("%s\n", parts[i])
+			res := r.FindStringSubmatch(parts[i])
+			names := r.SubexpNames()
+			for i, _ := range res {
+				if i != 0 {
+					fmt.Println(names[i], res[i])
+				}
+			}
+		}
 	}
 	//log.Printf("%d", n)
 }
